d9202099f43e7dcbe2f24a928eead9a86f0b304b
cookies-Dog <1091500906@qq.com>
2020-05-30 14:05:15 +0800

    add 1 files

--- node/config.js
+++ node/config.js
@@ -3,15 +3,15 @@ const path=require('path');
 module.exports={
 	//服务器端口
 	port:8080,
-	MD5_SUFFIX: 'wefnskdjbfskdbfkNwfk156164acs%$',
-	my_token:'1ad1a1f31a3d1a3sd1sad5sad1a31d3a$^$^$',
+	MD5_SUFFIX: 'xxxxxxx',
+	my_token:'xxxxxxxxx',
 
 	//数据库
-	db_host:'localhost',
-	db_user:'root',
+	db_host:'your host',
+	db_user:'your mysql user',
 	db_port:3306,
-	db_pass:'125800',
-	db_name:'mobile',
+	db_pass:'your mysql password',
+	db_name:'database',
 
 	//redis
 	redis_host:'localhost',
